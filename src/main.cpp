a3c45d8496002cfff20fc4830df642f861579632
AndreyHP <andreymarins501@gmail.com>
2025-06-11 01:04:41 -0300

    draw multiple cubes

--- src/main.cpp
+++ src/main.cpp
@@ -121,6 +121,20 @@ int main(int argc, char *argv[]) {
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VBO, VAO;
 
     glGenVertexArrays(1, &VAO);
@@ -137,13 +151,6 @@ int main(int argc, char *argv[]) {
     // texture coord attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-   //unsigned int indices[] = {  
-     //   0, 1, 3, // first triangle
-       // 1, 2, 3  // second triangle
-   // };
-
-   
 
     // load and create a texture 
     // -------------------------
@@ -230,7 +237,6 @@ int main(int argc, char *argv[]) {
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         }
 
-        // bind Texture
         // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
@@ -247,24 +253,38 @@ int main(int argc, char *argv[]) {
         glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
         
-        model = glm::rotate(model, (float)now, glm::vec3(1.0f, 0.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(Defaultshader.ID, "model");
         unsigned int viewLoc  = glGetUniformLocation(Defaultshader.ID, "view");
+
         // pass them to the shaders (3 different ways)
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+
         Defaultshader.setMat4("projection", projection);
-  
-        // Draw
+        Defaultshader.setMat4("view", view);
+        
+        // render boxes
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+           
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * now;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            Defaultshader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        
         // Swap buffers
         SDL_GL_SwapWindow(window);
+
     }
 
     // Cleanup
