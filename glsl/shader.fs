460f3c8da83a891ef2b326611fdf164f568cdeb4
AndreyHP <andreymarins501@gmail.com>
2025-07-05 22:50:06 -0300

    remove dithering from model

--- glsl/shader.fs
+++ glsl/shader.fs
@@ -8,9 +8,7 @@ float near = 0.1;
 float far  = 20.0;
 
 uniform sampler2D texture_diffuse1;
-uniform sampler2D ditherTex; // 4x4 Bayer matrix texture
 vec2 screenPos = gl_FragCoord.xy;
-float dither = texture2D(ditherTex, screenPos / 4.0).r;
 
 
 float LinearizeDepth(float depth)
@@ -26,7 +24,7 @@ void main()
 
     vec4 baseColor = texture(texture_diffuse1, TexCoords);
     baseColor.rgb = floor(baseColor.rgb * 16.0) / 16.0; // Reduce to 16 colors
-    baseColor = floor(( baseColor + dither * 0.1) * 32.0) / 32.0; // Apply dithering and Simulate 5-bit per channel (15-bit color)
+    baseColor = floor( baseColor * 32.0) / 32.0; //Simulate 5-bit per channel (15-bit color)
     fog = vec4(vec3(depth), 1.0);
 
     FragColor = mix(baseColor, vec4(1.0), fog);
